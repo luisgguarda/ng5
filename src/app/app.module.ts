f4cb3e20de0209ca6d8ba29fd3bf51ebd8ee76c2
luisgguarda <luisgguarda@outlook.com>
2017-11-19 11:47:07 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
+// Add animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//add Material
+import { MaterialModule } from './material/material.module';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+// Add Service
+import { DataService } from './data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
